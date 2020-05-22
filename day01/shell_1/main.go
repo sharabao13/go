e447f1901abc19994fe044dfde66d38a18388587
37203069@qq.com <sharahong13@gmail.com>
2020-05-22 20:32:54 +0800

    上传go 代码

---
+++ day01/shell_1/main.go
@@ -0,0 +1,19 @@
+// shell 脚本练习
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("touch", "test_file")
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Execute Command failed:" + err.Error())
+		return
+	}
+
+	fmt.Println("Execute Command finished.")
+}
